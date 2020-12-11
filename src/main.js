1000aa677a51600662ac949b7e7c660c4768b887
jimena campos <jimena@ucol.mx>
2020-12-11 02:35:06 -0600

    sumatoriaSerieUno implemented

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
-export default class App {}
+export default class App {
+
+    sumatoriaSerieUno(numero){
+
+        let suma = 0;
+        let i = 1;
+
+        for(let i = 1; i <= numero; i++ ){
+            suma = suma + (1 / i);
+        }
+        return suma;
+    }
+
+
+}
+
 
 let app = new App();
+
+
+console.log(app.sumatoriaSerieUno(1));
+console.log(app.sumatoriaSerieUno(2));
+console.log(app.sumatoriaSerieUno(3));
+console.log(app.sumatoriaSerieUno(4));
+console.log(app.sumatoriaSerieUno(5));
